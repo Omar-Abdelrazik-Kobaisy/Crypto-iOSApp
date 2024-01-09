81b7bfcc25c670e2af39c2e196718eee63efc57b
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2024-01-09 22:56:20 +0200

    finish CoinRowView

--- Crypto-Currency/Core/Home/Views/CoinRowView.swift
+++ Crypto-Currency/Core/Home/Views/CoinRowView.swift
@@ -9,34 +9,67 @@ import SwiftUI
 
 struct CoinRowView: View {
     let coin: CoinModel
+    let showHoldingsColumn: Bool
     var body: some View {
         HStack{
+            leftColumn
+            Spacer()
+            if showHoldingsColumn{
+                centerCoulmn
+            }
+            rightCoulmn
+        }
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group{
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension CoinRowView{
+    
+    private var leftColumn: some View{
+        HStack{
             Text("\(coin.rank)")
-                .font(.caption)
+                .font(.headline)
                 .foregroundColor(.theme.secondaryText)
             Circle()
                 .frame(width:30,height: 30)
             Text((coin.symbol ?? "noSymbol").uppercased())
                 .font(.headline)
                 .foregroundColor(.theme.accent)
-            Spacer()
-            VStack(alignment: .trailing){
-                Text((coin.currentPrice ?? 0.00).toCurrencyFormate6())
-                    .bold()
-                    .foregroundColor(.theme.accent)
-                Text((coin.priceChangePercentage24H ?? 0).toCurrencyFormate6())
-                    .foregroundColor(
-                        coin.priceChangePercentage24H ?? 0 >= 0 ?
-                            .theme.green : .theme.red
-                    )
-            }
         }
-        .padding(.horizontal)
     }
-}
-
-struct CoinRowView_Previews: PreviewProvider {
-    static var previews: some View {
-        CoinRowView(coin: dev.coin)
+    
+    private var centerCoulmn: some View{
+            VStack(alignment:.trailing){
+                Text(coin.currentHoldingPrice.toCurrencyFormate2())
+                    .bold()
+                Text(coin.currentHoldings?.asNumberString() ?? "0.00")
+            }
+            .foregroundColor(.theme.accent)
+    }
+    
+    private var rightCoulmn: some View{
+        VStack(alignment: .trailing){
+            Text((coin.currentPrice ?? 0.00).toCurrencyFormate6())
+                .bold()
+                .foregroundColor(.theme.accent)
+            Text((coin.priceChangePercentage24H ?? 0).asPercentString())
+                .foregroundColor(
+                    coin.priceChangePercentage24H ?? 0 >= 0 ?
+                        .theme.green : .theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
     }
 }
