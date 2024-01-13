4d83389f50c134a43359cb87eb1beae1065eb0e9
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2024-01-13 23:48:34 +0200

    FetchCoinDataFromWebServer +Add CoinDataService

--- Crypto-Currency/Core/Home/ViewModel/HomeViewModel.swift
+++ Crypto-Currency/Core/Home/ViewModel/HomeViewModel.swift
@@ -6,16 +6,40 @@
 //
 
 import Foundation
-
+import Combine
 class HomeViewModel: ObservableObject{
-    @Published var livePriceCoins: [CoinModel] = []
-    @Published var portfolioCoins: [CoinModel] = []
+//    @Published var livePriceCoins: [CoinModel] = []
+//    @Published var portfolioCoins: [CoinModel] = []
+    @Published var allCoins: [CoinModel] = []
+    private let dataService: CoinDataServiceProtocol
+//    var coinSubscription: AnyCancellable?
+    var cancel = Set<AnyCancellable>()
     
-    init(){
-        DispatchQueue.main.async {
-            self.livePriceCoins.append(DeveloperPreview.instance.coin)
-            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-        }
-        
+    init(dataService: CoinDataServiceProtocol = CoinDataService()){
+        self.dataService = dataService
+        addSubscribers()
+//        DispatchQueue.main.async {[weak self] in
+//            self?.livePriceCoins.append(DeveloperPreview.instance.coin)
+//            self?.portfolioCoins.append(DeveloperPreview.instance.coin)
+//        }
+    }
+    
+    func addSubscribers(){
+        dataService.getCoins()
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("Successfully Decoded data into CoinModel")
+                case .failure(let error):
+                    print("can't decode data into CoinModel ->"+error.localizedDescription)
+                }
+            } receiveValue: {[weak self] coins in
+                guard let self else{return}
+                self.allCoins = coins
+                print(self.allCoins)
+//                self.coinSubscription?.cancel()
+            }
+            .store(in: &cancel)
+
     }
 }
