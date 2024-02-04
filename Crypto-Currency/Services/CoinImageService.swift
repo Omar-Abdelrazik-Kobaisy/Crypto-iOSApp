0f63f824a27262983d7f7b474278ae6e18e5e74e
omarAbdelrazik <omar.abdelrazik98@gmail.com>
2024-02-04 07:19:42 +0200

    implementOfflineStorageUsingfileManager +Add LocalFIleManager

--- Crypto-Currency/Services/CoinImageService.swift
+++ Crypto-Currency/Services/CoinImageService.swift
@@ -11,8 +11,10 @@ import Combine
 
 final class CoinImageService{
     @Published var image: UIImage? = nil
-    let coin: CoinModel
-    var networkManager: NetworkProtocol
+    private let coin: CoinModel
+    private var networkManager: NetworkProtocol
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "CoinImage"
     private var coinImageSubscription: AnyCancellable?
     init(networkManager: NetworkProtocol = NetworkManager(),
          coin: CoinModel){
@@ -20,7 +22,15 @@ final class CoinImageService{
         self.coin = coin
         getImage()
     }
-    func getImage(){
+    private func getImage(){
+        if let savedImage = fileManager.getImage(imageName: coin.id ?? "0", folderName: folderName)  {
+            image = savedImage
+            print("imageRetrieved from fileManager")
+        }else{
+            downloadCoinImage()
+        }
+    }
+    private func downloadCoinImage(){
         guard let url = URL(string: coin.image ?? "") else{
             print("bad URL")
             return
@@ -31,8 +41,11 @@ final class CoinImageService{
                 return UIImage(data: output)
             })
             .sink(receiveCompletion: networkManager.handleCompletion(completion:), receiveValue: {[weak self] image in
-                guard let self else{return}
+                guard let self, let image else{return}
                 self.image = image
+                self.fileManager.saveImage(image: image ,
+                                           imageName: self.coin.id ?? "0",
+                                           folderName: self.folderName)
                 self.coinImageSubscription?.cancel()
             })
     }
